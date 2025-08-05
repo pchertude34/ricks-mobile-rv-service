c51d338a585df421466e871757845746fc151692
Payton Chertude <p.chertude34@gmail.com>
2025-08-04 18:32:26 -0700

    header and init

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,23 +1,24 @@
 module.exports = {
-    name: "Code Stitch Web Designs",
-    email: "help@codestitch.app",
-    phoneForTel: "555-779-4407",
-    phoneFormatted: "(555) 779-4407",
-    address: {
-        lineOne: "First Address Line",
-        lineTwo: "Second Address Line",
-        city: "Denver",
-        state: "CO",
-        zip: "80206",
-        country: "US",
-        mapLink: "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6",
-    },
-    socials: {
-        facebook: "https://www.facebook.com/",
-        instagram: "https://www.instagram.com/",
-    },
-    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    domain: "https://www.example.com",
-    // Passing the isProduction variable for use in HTML templates
-    isProduction: process.env.ELEVENTY_ENV === "PROD",
+  name: "Code Stitch Web Designs",
+  email: "help@codestitch.app",
+  phoneForTel: "555-779-4407",
+  phoneFormatted: "(555) 779-4407",
+  address: {
+    lineOne: "First Address Line",
+    lineTwo: "Second Address Line",
+    city: "Denver",
+    state: "CO",
+    zip: "80206",
+    country: "US",
+    mapLink: "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6",
+  },
+  socials: {
+    facebook: "https://www.facebook.com/",
+    instagram: "https://www.instagram.com/",
+  },
+  hours: "7 days a week 9-7 (Winter 9-5)",
+  //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+  domain: "https://www.example.com",
+  // Passing the isProduction variable for use in HTML templates
+  isProduction: process.env.ELEVENTY_ENV === "PROD",
 };
